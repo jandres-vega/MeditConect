abbd6f184bb101549a014a0280e3fb573b15c153
jandres-vega <jandresvega@itsa.edu.co>
2023-04-04 18:57:47 -0500

    Feact: add estructura de proyecto api-rest y client

---
+++ API-REST/index.js
@@ -0,0 +1,5 @@
+const {app} = require('./src/App');
+
+app.listen(3005, () => {
+    console.log(`listen on port ${3005}`)
+})
---
+++ API-REST/src/App.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const {routes} = require('./routes/index');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json())
+
+routes(app);
+
+module.exports = {app}
+
+
